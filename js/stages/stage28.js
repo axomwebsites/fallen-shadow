c2785980e35404e6df40fa54623c06e13f3dfe98
axom websites <adamowaissi44@gmail.com>
2026-06-10 10:06:50 +0300

    Create stage28.js

---
+++ js/stages/stage28.js
@@ -0,0 +1,7 @@
+export const width = 6700;
+export function init(p, h, e, g, b) {
+p.push({x:0,y:450,w:700,h:200},{x:850,y:360,w:500,h:290},{x:1500,y:440,w:900,h:210},{x:2550,y:330,w:450,h:320},{x:3150,y:410,w:850,h:240},{x:4150,y:340,w:450,h:310},{x:4800,y:450,w:1900,h:200});
+h.push({x:1000,y:345,w:60,h:15,type:"spike"},{x:1750,y:425,w:80,h:15,type:"spike"},{x:2700,y:315,w:40,h:15,type:"spike"},{x:3450,y:395,w:80,h:15,type:"spike"});
+e.push({x:1650,y:400,minx:1520,maxx:2350,vx:5.2,w:40,h:40,dead:false},{x:3400,y:370,minx:3170,maxx:3950,vx:5.8,w:40,h:40,dead:false});
+g.push({x:6500,y:390,w:50,h:60});
+}
