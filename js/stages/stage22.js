7c7e2b8d8a600d0b97d894450944dc1df0e08d18
axom websites <adamowaissi44@gmail.com>
2026-06-10 10:04:57 +0300

    Create stage22.js

---
+++ js/stages/stage22.js
@@ -0,0 +1,7 @@
+export const width = 6100;
+export function init(p, h, e, g, b) {
+p.push({x:0,y:450,w:550,h:200},{x:700,y:340,w:450,h:310},{x:1300,y:420,w:850,h:210},{x:2300,y:310,w:550,h:340},{x:3000,y:390,w:750,h:260},{x:3900,y:320,w:550,h:310},{x:4600,y:450,w:1500,h:200});
+h.push({x:850,y:325,w:40,h:15,type:"spike"},{x:1500,y:405,w:80,h:15,type:"spike"},{x:2450,y:295,w:60,h:15,type:"spike"},{x:3300,y:375,w:60,h:15,type:"spike"});
+e.push({x:1450,y:380,minx:1320,maxx:2100,vx:4.6,w:40,h:40,dead:false},{x:3200,y:350,minx:3020,maxx:3700,vx:5.2,w:40,h:40,dead:false});
+g.push({x:5900,y:390,w:50,h:60});
+}
