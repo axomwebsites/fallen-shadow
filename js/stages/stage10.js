06973f82a21deb1d4d10a87444b617599e33a95f
axom websites <adamowaissi44@gmail.com>
2026-06-10 08:38:57 +0300

    Create stage10.js

---
+++ js/stages/stage10.js
@@ -0,0 +1,5 @@
+export const width = 2000;
+export function init(p, h, e, g, b) {
+p.push({x:0,y:450,w:2000,h:200},{x:450,y:310,w:200,h:20},{x:1000,y:310,w:200,h:20});
+b.push({x:1500,y:340,w:90,h:110,hp:8,maxhp:8,ticks:0,state:"idle",type:"normal"});
+}
