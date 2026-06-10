8b89ba258dc6545a5e14e77c15293c3688fb916d
axom websites <adamowaissi44@gmail.com>
2026-06-10 10:05:12 +0300

    Create stage23.js

---
+++ js/stages/stage23.js
@@ -0,0 +1,7 @@
+export const width = 6200;
+export function init(p, h, e, g, b) {
+p.push({x:0,y:450,w:650,h:200},{x:800,y:360,w:500,h:290},{x:1450,y:440,w:900,h:200},{x:2500,y:330,w:450,h:320},{x:3100,y:410,w:850,h:240},{x:4100,y:340,w:450,h:310},{x:4750,y:450,w:1450,h:200});
+h.push({x:950,y:345,w:60,h:15,type:"spike"},{x:1700,y:425,w:80,h:15,type:"spike"},{x:2650,y:315,w:40,h:15,type:"spike"},{x:3450,y:395,w:80,h:15,type:"spike"});
+e.push({x:1600,y:400,minx:1470,maxx:2300,vx:4.7,w:40,h:40,dead:false},{x:3350,y:370,minx:3120,maxx:3900,vx:5.3,w:40,h:40,dead:false});
+g.push({x:6000,y:390,w:50,h:60});
+}
